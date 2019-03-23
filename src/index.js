bb279bbba8f721a27bb88dfd334a4639f8cd69d8
oussou-dev <oussou.dev@gmail.com>
2019-03-23 15:29:33 +0100

    Create store with function createStore from redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-
 import "./styles.css"
+import { createStore } from "redux"
+import { todosReducer } from "./store/reducers"
+
+const store = createStore(todosReducer)
 
 const rootElement = document.getElementById("root")
 ReactDOM.render(<App />, rootElement)
