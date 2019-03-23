adbacc9a70c5818aac6e8e81fdbf7e52989a8087
oussou-dev <oussou.dev@gmail.com>
2019-03-23 16:51:50 +0100

    Connect & MapStateToProps

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-const TodoItem = props => {
+const TodoItem = ({ todo }) => {
 	return (
 		<li className="list-group-item d-flex flex-row justify-content-between aligns-item-center my-1">
-			<span>x</span>
+			<span>{todo.name}</span>
 			<span>
 				<input className="mx-3" type="checkbox" />
 				<button className="btn btn-sm btn-danger mr-3">delete</button>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,40 @@
 import React from "react"
 import TodoItem from "../components/TodoItem"
+import { connect } from "react-redux"
+import { visibilityFilters } from "../store/actions"
 
-const TodoList = props => {
+const TodoList = ({ todos }) => {
 	return (
 		<ul className="list-group">
-			<TodoItem />
-			<TodoItem />
+			{todos &&
+				todos.map((t, i) => <TodoItem key={t.name + i} todo={t} />)}
 		</ul>
 	)
 }
 
-export default TodoList
+export default connect(state => {
+	const filter = state.filter
+	let todos = []
+	switch (filter) {
+		case visibilityFilters.SHOW_ALL:
+			{
+				todos = state.todos
+			}
+			break
+		case visibilityFilters.SHOW_DONE:
+			{
+				todos = state.todos.filter(t => t.done)
+			}
+			break
+		case visibilityFilters.SHOW_ACTIVE: {
+			todos = state.todos.filter(t => !t.done)
+		}
+		default: {
+			todos = state.todos
+		}
+	}
+
+	return {
+		todos
+	}
+})(TodoList)
