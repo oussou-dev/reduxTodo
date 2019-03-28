3c8c364cdb973fca406f6d236fc17a7dee19e764
oussou-dev <oussou.dev@gmail.com>
2019-03-28 16:20:48 +0100

    toggleTodo & deleteTodo done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,23 @@
 import React from "react"
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, toggleTodo, deleteTodo }) => {
 	return (
-		<li className="list-group-item d-flex flex-row justify-content-between aligns-item-center my-1">
+		<li
+			onClick={toggleTodo}
+			className="list-group-item d-flex flex-row justify-content-between aligns-item-center my-1"
+		>
 			<span>{todo.name}</span>
 			<span>
 				<input className="mx-3" type="checkbox" />
-				<button className="btn btn-sm btn-danger mr-3">delete</button>
+				<button
+					onClick={e => {
+						e.stopPropagation()
+						deleteTodo()
+					}}
+					className="btn btn-sm btn-danger mr-3"
+				>
+					delete
+				</button>
 			</span>
 		</li>
 	)
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,41 +1,60 @@
 import React from "react"
 import TodoItem from "../components/TodoItem"
 import { connect } from "react-redux"
-import { visibilityFilters } from "../store/actions"
+import {
+	visibilityFilters,
+	toggleTodo,
+	deleteTodo
+} from "../store/actions"
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, deleteTodo, toggleTodo }) => {
 	return (
 		<ul className="list-group">
 			{todos &&
-				todos.map((t, i) => <TodoItem key={t.name + i} todo={t} />)}
+				todos.map((t, i) => (
+					<TodoItem
+						key={t.name}
+						todo={t}
+						deleteTodo={() => deleteTodo(i)}
+						toggleTodo={() => toggleTodo(i)}
+					/>
+				))}
 		</ul>
 	)
 }
 
-export default connect(state => {
-	// console.log(state)
-	const filter = state.filter
-	let todos = []
-	switch (filter) {
-		case visibilityFilters.SHOW_ALL:
-			{
+export default connect(
+	state => {
+		// console.log(state)
+		const filter = state.filter
+		let todos = []
+		switch (filter) {
+			case visibilityFilters.SHOW_ALL:
+				{
+					todos = state.todos
+				}
+				break
+			case visibilityFilters.SHOW_DONE:
+				{
+					todos = state.todos.filter(t => t.done)
+				}
+				break
+			case visibilityFilters.SHOW_ACTIVE: {
+				todos = state.todos.filter(t => !t.done)
+				break
+			}
+			default: {
 				todos = state.todos
+				break
 			}
-			break
-		case visibilityFilters.SHOW_DONE:
-			{
-				todos = state.todos.filter(t => t.done)
-			}
-			break
-		case visibilityFilters.SHOW_ACTIVE: {
-			todos = state.todos.filter(t => !t.done)
 		}
-		default: {
-			todos = state.todos
-		}
-	}
 
-	return {
-		todos
+		return {
+			todos
+		}
+	},
+	{
+		toggleTodo,
+		deleteTodo
 	}
-})(TodoList)
+)(TodoList)
